9f43e2c3febfd43399ba6b5db6079dbd4f646b67
jjfigueroa <josejaime.figueroa@molex.com>
2020-11-07 16:37:38 -0700

    First configuration.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const cron = require('node-cron');
 const execCronJob = require('./utils/fetch');
+let cronTime = process.env.CRON_SCHEDULE || '* */15 * * * *';
 
-cron.schedule('*/3 * * * * *', () => {
+console.log('Cron job started at ' + cronTime);
+
+cron.schedule(cronTime, () => {
+  console.log('About to execute jobs');
   // execCronJob();
-  console.log('Cada segundo');
+  console.log('Done executing jobs');
 });
--- utils/fetch.js
+++ utils/fetch.js
@@ -1,13 +1,21 @@
 const fetch = require('node-fetch');
 
 const fetchObject = [
-  { url: 'http://localhost:5000/Charge/ApplyMembershipCharges', method: 'Get' },
-  { url: 'https://api.github.com/users', method: 'Get' },
+  { url: 'http://localhost:5000/Charge/ApplyMembershipCharges.json', method: 'Get' },
+  // { url: 'https://api.github.com/users', method: 'Get' },
+  // { url: 'http://localhost:5000/Ping.json', method: 'Get' },
 ];
 
 async function execCronJob() {
   for (const URL of fetchObject) {
-    await fetch(URL.url);
+    try {
+      console.log(`About to get ${URL.url}`);
+      const resp = await fetch(URL.url);
+      const json = await resp.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return;
